41ac04460773453c19260252c268cb6c62df0c59
sl6004 <sl6004@outlook.com>
2015-06-17 19:17:49 -0600

    Update server.R file

--- ui.R
+++ ui.R
@@ -16,7 +16,7 @@ shinyUI(
     sidebarPanel
     (
       h4('Please enter your data below\n'),
-      checkboxGroupInput("am","Transmission Type",c('Automatic ("0")'=0,'Manual ("1")'=1)),
+      checkboxGroupInput("am","Transmission Type (Select only one type)",c('Automatic ("0")'=0,'Manual ("1")'=1)),
       numericInput('wt','Weight*1000lb (min=1,max=6,step=0.5)',0,min=1,max=6,step=0.5),
       numericInput('hp','Horse Power (min=52, max=335, step=10)', 52, min=52, max=335, step=10),
       submitButton('Submit')
@@ -32,8 +32,8 @@ shinyUI(
         ("Application Information",
          
          h4('This is a simple Shiny App using multivariate Regression model to predict car fuel efficiency - Mileage Per Gallon (MPG).'),
-         h4('Based on the previously analysis we select the model requires three major variables: weight, horse power, and transmission type.'),
-         h4('To use this application, please enter these three required variables on the left panel. The App may take a few seconds to calculate after data entered.'),
+         h4('Based on the previously analysis we have selected the model requires three major variables: transmission type, weight, and horse power.'),
+         h4('To use this application, please enter these three required variables on the left panel, and click the "Submit" botton when you are done.  The App may take a few seconds to calculate after data submitted'),
          h4('Please be patient and navigate to "Results" tab (on the top) to view the corresponding prediction results.'),
          p(strong(em("For more information, please visit:",a("http://rpubs.com/sl6004/shinydoc",href="http://rpubs.com/sl6004/shinydoc"))))     
         ),
